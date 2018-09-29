981cf5b3b502401da29a3d5218b7e72a44988250
anovasia <40883517+anovasia@users.noreply.github.com>
2018-09-29 16:11:59 +1200

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ database: "sql12247448"
     console.log(result2);
   
     return res.json({
-        fulfillmentText: speech+"1",
+        fulfillmentText: speech+result2,
         payload: {
             skype: {
                 expectUserResponse: true,
