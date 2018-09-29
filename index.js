c41ea906e5b08c3150c645f27dd27aec67ca4c74
anovasia <40883517+anovasia@users.noreply.github.com>
2018-09-29 20:26:55 +1200

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ database: "sql12247448"
     console.log(result2);
   
     return res.json({
-        fulfillmentText: speech+"\n"+result2,
+        fulfillmentText: speech,
       
         payload: {
             google: {
