da2afb3a947d724b448b716516e905d9b821ea6e
anovasia <40883517+anovasia@users.noreply.github.com>
2018-10-03 15:20:22 +1300

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ restService.post("/echo", function(req, res) {
         req.body.queryResult.parameters &&
         req.body.queryResult.parameters.echoText
             ? req.body.queryResult.parameters.echoText
-            : "1";
+            : "";
 
     console.log('speech:', speech);
  var qstring=speech;
